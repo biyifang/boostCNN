02d4f5c56e7c51c6bf9d9f2734a10324c144f9d7
biyifang <57164923+biyifang@users.noreply.github.com>
2019-11-09 12:22:36 -0600

    Update pre_trained_boostCNN.py

--- pre_trained_boostCNN.py
+++ pre_trained_boostCNN.py
@@ -504,7 +504,8 @@ def train_boost( train_loader_seq, weight_loader, weight_dataset, train_dataset,
 
 
             # compute output
-            loss = model(images, weight, k)        
+            loss = model(images, weight, k) 
+            print(loss)       
 
             # measure accuracy and record loss
             #acc1, acc5 = accuracy(output, target, topk=(1, 5))
