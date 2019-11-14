ecf5d901eb7d096e7aaf11ce12079fbbfe646207
biyifang <57164923+biyifang@users.noreply.github.com>
2019-11-13 19:45:37 -0600

    Update pre_trained_boostCNN.py

--- pre_trained_boostCNN.py
+++ pre_trained_boostCNN.py
@@ -45,7 +45,7 @@ parser.add_argument('--num_boost_iter', default=50, type=int, metavar='N',
                     help='number of boosting iterations')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=128, type=int,
+parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
