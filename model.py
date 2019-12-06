0eb664d7e230ec0e4fa45fd18cb0dfabdd8f26df
biyifang <57164923+biyifang@users.noreply.github.com>
2019-12-06 10:32:05 -0600

    Update model.py

--- model.py
+++ model.py
@@ -61,40 +61,40 @@ class oneCNN(nn.Module):
 '''
 
 class oneCNN(nn.Module):
-    def __init__(self, num_classes=10):
-        super(oneCNN, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+	def __init__(self, num_classes=10):
+		super(oneCNN, self).__init__()
+		self.features = nn.Sequential(
+			nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+			nn.Conv2d(64, 192, kernel_size=5, padding=2),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+			nn.Conv2d(192, 384, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(384, 256, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+		)
+		self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+		self.classifier = nn.Sequential(
+			nn.Dropout(),
+			nn.Linear(256 * 6 * 6, 4096),
+			nn.ReLU(inplace=True),
+			nn.Dropout(),
+			nn.Linear(4096, 4096),
+			nn.ReLU(inplace=True),
+			nn.Linear(4096, num_classes),
+		)
 
-    def forward(self, x, label=None, temperature=None):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        if label is not None:
+	def forward(self, x, label=None, temperature=None):
+		x = self.features(x)
+		x = self.avgpool(x)
+		x = torch.flatten(x, 1)
+		x = self.classifier(x)
+		if label is not None:
 			loss = torch.sum(nn.functional.softmax(label, -1)*nn.functional.log_softmax(x_1/temperature,-1), dim=1).mean()
 			return -1.0*loss
 		else:
