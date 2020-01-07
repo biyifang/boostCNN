5d2e112bb73c03169f0d8de7bee3d027b2c84c62
biyifang <57164923+biyifang@users.noreply.github.com>
2020-01-07 11:29:14 -0600

    update

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class oneCNN(nn.Module):
 			#nn.Conv2d(3, 16, kernel_size=32, stride=4, padding=2),
 			#nn.Conv2d(3, 16, kernel_size=16, stride=4, padding=2),
 			#nn.BatchNorm2d(16),
-			nn.Dropout(p=0.2),
+			#nn.Dropout(p=0.2),
 			nn.Conv2d(3, 64, kernel_size=16, stride=4, padding=2),
 			nn.BatchNorm2d(64),
 			nn.ReLU(inplace=True),
@@ -19,6 +19,7 @@ class oneCNN(nn.Module):
 			nn.MaxPool2d(kernel_size=3, stride=2))
 		self.features_2 = nn.Sequential(
 			#nn.Conv2d(16, 4, kernel_size=8, stride=4, padding=2),
+			nn.Dropout(p=0.2),
 			nn.Conv2d(64, 4, kernel_size=8, stride=4, padding=2),
 			nn.BatchNorm2d(4),
 			nn.ReLU(inplace=True),
