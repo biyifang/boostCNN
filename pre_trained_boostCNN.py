19eb0f7451ce3dae082052b7683180979137172e
biyifang <57164923+biyifang@users.noreply.github.com>
2020-01-13 19:17:40 -0600

    Update pre_trained_boostCNN.py

--- pre_trained_boostCNN.py
+++ pre_trained_boostCNN.py
@@ -397,8 +397,9 @@ def main_worker(gpu, ngpus_per_node, args):
 
 
 	model_2 = torch.load('initial_model_' + args.model_save)
-	model_2 = oneCNN_two()
-	model_list = [copy.deepcopy(model_2) for _ in range(args.num_boost_iter)]
+	#model_list = [copy.deepcopy(model_2) for _ in range(args.num_boost_iter)]
+	model_2_1 = oneCNN_two()
+	model_list = [copy.deepcopy(model_2)] + [ copy.deepcopy(model_2_1) for _ in range(args.num_boost_iter)]
 	model_3 = GBM(args.num_boost_iter, args.boost_shrink, model_list)
 	model_3.cpu()
 	model_3.train()
@@ -565,17 +566,16 @@ def train_boost( train_loader_seq, weight_loader, weight_dataset, train_dataset,
 			# measure data loading time
 			data_time.update(time.time() - end)
 
-			
-			if k == 0:
+			print(images.size())
+			if k == 1:
 				images = images[:, :168, :168,:]
-			'''
 			elif k == 2:
-				images = images[]
+				images = images[:, :168, 56:, :]
 			elif k == 3:
-				images = images[]
+				images = images[:, 56:, :169, :]
 			elif k == 4:
-				images = images[]
-			'''
+				images = images[:, 56:, 56:, :]
+			
 
 			images = images.cuda()
 			weight = weight.cuda()
@@ -636,6 +636,16 @@ def validate_boost(val_loader, model, criterion, args, k):
 	with torch.no_grad():
 		end = time.time()
 		for i, (images, target) in enumerate(val_loader):
+
+			if k == 1:
+				images = images[:, :168, :168,:]
+			elif k == 2:
+				images = images[:, :168, 56:, :]
+			elif k == 3:
+				images = images[:, 56:, :169, :]
+			elif k == 4:
+				images = images[:, 56:, 56:, :]
+				
 			images = images.cuda()
 			target = target.cuda()
 
