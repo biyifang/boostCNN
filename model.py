77f6f27c71ac28b6dd9df2353364988b1b9cd49f
biyifang <57164923+biyifang@users.noreply.github.com>
2020-01-22 15:37:46 -0600

    Update model.py

--- model.py
+++ model.py
@@ -58,16 +58,16 @@ class oneCNN(nn.Module):
 		#self.res = nn.Linear(16*26*26, 4*3*3)
 		#self.res = nn.Linear(64*26*26, 4*3*3)
 		#3-layers
-		self.res = nn.Linear(128*28*28, 32*3*3)
+		self.res = nn.Linear(128*27*27, 32*3*3)
 		self.mse = nn.MSELoss()
 	def forward(self, x, label=None, temperature=None, if_student = True):
 		x_1 = self.features_1(x)
 		x_f = torch.flatten(x_1, 1)
-		print(x_f.size())
+		#print(x_f.size())
 		x_res = self.res(x_f)
 		x_1 = self.features_2(x_1)
 		x_1 = torch.flatten(x_1, 1)
-		print(x_1.size())
+		#print(x_1.size())
 		x_1 = self.classifier(x_1 + x_res)
 		#x_1 = self.classifier(x_1)
 		if not if_student:
