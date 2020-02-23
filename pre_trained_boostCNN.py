8c2a17c4b4efb4e9c6bd530aa7edd11523877c11
biyifang <57164923+biyifang@users.noreply.github.com>
2020-02-22 23:55:58 -0600

    up

--- pre_trained_boostCNN.py
+++ pre_trained_boostCNN.py
@@ -169,8 +169,8 @@ def main_worker(gpu, ngpus_per_node, args):
 		print("=> creating model '{}'".format(args.arch))
 		model = models.__dict__[args.arch]()
 		#model = models.resnet18(num_classes=10)
-		#model = resNet18()
-		model = mobilenet_v2()
+		model = resNet18()
+		#model = mobilenet_v2()
 		#model = oneCNN()
 		model.cuda()
 
