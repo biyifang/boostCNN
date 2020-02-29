4dc684a645101caf82a33aba332c80d660237345
biyifang <57164923+biyifang@users.noreply.github.com>
2020-02-28 22:24:27 -0600

    Update subgrid_boostCNN.py

--- subgrid_boostCNN.py
+++ subgrid_boostCNN.py
@@ -81,7 +81,7 @@ parser.add_argument('--lr_dis', '--learning-rate-dis', default=0.001, type=float
 parser.add_argument('--lr_boost', '--learning-rate-boost', default=0.00001, type=float,
 					metavar='LRboost', help='learning rate for boosting', dest='lr_boost')
 parser.add_argument('--lr_sub', default=0.00001, type=float,
-					metavar='LRsubgrid', help='learning rate for subgrid training', dest='lr_boost')
+					metavar='LRsubgrid', help='learning rate for subgrid training')
 parser.add_argument('--temperature', '--temperature', default=3.0, type=float,
 					metavar='temperature', help='temperature for softmax', dest='temperature')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
@@ -369,7 +369,7 @@ def main_worker(gpu, ngpus_per_node, args):
 	'''
 	
 
-	
+	'''
 	#if have teacher model, no need to run step one
 	model = torch.load('teacher_model_resnet18')
 	_, new_predict = validate(train_loader, model, criterion, args, True)
@@ -429,7 +429,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
 	# boosted CNN
 	model_2.cpu()
-	
+	'''
+
 	#model.cpu()
 	output_file = open('out.txt','w')
 	
@@ -437,7 +438,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
 
 	#Create module for GBM
-	#model_2 = torch.load('initial_model_' + args.model_save)
+	model_2 = torch.load('initial_model_' + args.model_save)
+	model.cpu()
 	#model_list = [copy.deepcopy(model_2) for _ in range(args.num_boost_iter)]
 	#model_2 = oneCNN()
 	#model_2 = mobilenet_v2()
