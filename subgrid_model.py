04bf45362b7326683e3a2d7398d16abbba34843e
biyifang <57164923+biyifang@users.noreply.github.com>
2020-03-09 15:13:46 -0500

    Update subgrid_model.py

--- subgrid_model.py
+++ subgrid_model.py
@@ -561,7 +561,7 @@ class GBM(nn.Module):
         #data = TensorDataset(f,g,label) sequntial data
         lower = 0.0
         upper = 1.0
-        merror = 1e-2
+        merror = 1e-5
         label = [ it[1]  for it in data ]
         num_classes = self.num_classes
         def obj(pred, label, num_classes):
