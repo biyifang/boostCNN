f2ad89db657fcc7fba33fbc6c2d16363cfebc761
biyifang <57164923+biyifang@users.noreply.github.com>
2020-04-16 20:59:19 -0500

    Update late_subgrid_boostCNN_resNet18.py

--- late_subgrid_boostCNN_resNet18.py
+++ late_subgrid_boostCNN_resNet18.py
@@ -611,7 +611,7 @@ def main_worker(gpu, ngpus_per_node, args):
 			'''
 			with torch.no_grad():
 				for i, (images, target) in enumerate(tqdm(train_loader_seq)):
-					images = images[:,:, x_axis,:][:,:,:,y_axis]
+					images = images[:,:, x_axis,:][:,:,:,y_axis].cuda()
 					new_size = model_3.weak_learners[k].get_size(images)
 					break
 			model_3.weak_learners[k].fc = nn.Linear(new_size, 10)
