48e90034273af514bd4b013a9c89395fac759447
biyifang <57164923+biyifang@users.noreply.github.com>
2020-04-19 10:52:54 -0500

    Update late_subgrid_boostCNN_resNet18.py

--- late_subgrid_boostCNN_resNet18.py
+++ late_subgrid_boostCNN_resNet18.py
@@ -251,7 +251,7 @@ def main_worker(gpu, ngpus_per_node, args):
 	#Normalization for MNIST
 	#normalize = transforms.Normalize(mean=[0.485], std=[0.229])
 
-	'''
+	
 	train_dataset = datasets.SVHN(args.data, split='train', transform=transforms.Compose([
 			transforms.RandomResizedCrop(224),
 			transforms.RandomHorizontalFlip(),
@@ -265,6 +265,7 @@ def main_worker(gpu, ngpus_per_node, args):
 			transforms.ToTensor(),
 			normalize,
 		]), target_transform=None, download=True)
+	'''
 
 	'''
 	train_dataset = datasets.MNIST(args.data, train=True, transform=transforms.Compose([
@@ -292,7 +293,7 @@ def main_worker(gpu, ngpus_per_node, args):
 	weight_loader = torch.utils.data.DataLoader(
 		 weight_dataset, batch_size=args.batch_size, sampler=weight_sampler)
 
-	'''
+	
 	val_dataset = datasets.SVHN(args.data, split='test', transform=transforms.Compose([
 			transforms.RandomResizedCrop(224, scale=(1.0, 1.0)),
 			transforms.ToTensor(),
@@ -306,7 +307,7 @@ def main_worker(gpu, ngpus_per_node, args):
 			transforms.ToTensor(),
 			normalize,
 		]), target_transform=None, download=False)
-	
+	'''
 	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
 		num_workers=args.workers, pin_memory=True)
 	probability = torch.zeros(len(val_dataset), args.num_class)
@@ -330,7 +331,7 @@ def main_worker(gpu, ngpus_per_node, args):
 		return
 	'''
 
-	'''
+	
 	#step one: find a good teacher model
 	if args.teacher_model_save:
 		model = torch.load('teacher_model_' + args.teacher_model_save)
@@ -350,6 +351,7 @@ def main_worker(gpu, ngpus_per_node, args):
 			# remember best acc@1 and save checkpoint
 			is_best = acc1 > best_acc1
 			best_acc1 = max(acc1, best_acc1)
+			'''
 			if acc1 == best_acc1:
 				_, new_predict = validate(train_loader, model, criterion, args, True)
 				new_predict = torch.cat(new_predict)
@@ -360,7 +362,7 @@ def main_worker(gpu, ngpus_per_node, args):
 				model.cpu()
 				torch.save(model, 'SVHN_teacher_model_resnet18')
 				model.cuda()
-
+			'''
 
 			if not args.multiprocessing_distributed or (args.multiprocessing_distributed
 					and args.rank % ngpus_per_node == 0):
@@ -380,7 +382,7 @@ def main_worker(gpu, ngpus_per_node, args):
 		print(best_acc1)
 		#l = input('l')
 	model.cpu()
-	'''
+	
 	
 
 	'''
