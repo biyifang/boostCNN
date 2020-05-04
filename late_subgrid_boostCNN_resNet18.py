7055ed2a893314a88e4d76640fbffdc476c03d1a
biyifang <57164923+biyifang@users.noreply.github.com>
2020-05-04 11:44:29 -0500

    Update late_subgrid_boostCNN_resNet18.py

--- late_subgrid_boostCNN_resNet18.py
+++ late_subgrid_boostCNN_resNet18.py
@@ -365,7 +365,7 @@ def main_worker(gpu, ngpus_per_node, args):
 		return
 	'''
 
-	'''
+	
 	#step one: find a good teacher model
 	if args.teacher_model_save:
 		model = torch.load('teacher_model_' + args.teacher_model_save)
@@ -416,7 +416,7 @@ def main_worker(gpu, ngpus_per_node, args):
 		print(best_acc1)
 		#l = input('l')
 	model.cpu()
-	'''
+	
 	
 
 	'''
