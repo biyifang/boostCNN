0fea006cfa60d4158e2a87bd8d739f3ec55bddb1
biyifang <57164923+biyifang@users.noreply.github.com>
2020-05-04 20:01:42 -0500

    Update late_subgrid_boostCNN_resNet18.py

--- late_subgrid_boostCNN_resNet18.py
+++ late_subgrid_boostCNN_resNet18.py
@@ -175,8 +175,7 @@ def main_worker(gpu, ngpus_per_node, args):
 	else:
 		print("=> creating model '{}'".format(args.arch))
 		model = models.__dict__[args.arch]()
-		model = models.resnet18(num_classes=args.num_class)
-		#, pretrained=True)
+		model = models.resnet18(num_classes=args.num_class, pretrained=True)
 		#model = ResNet(num_classes=100)
 		#model = mobilenet_v2()
 		#model = MobileNet_V2()
