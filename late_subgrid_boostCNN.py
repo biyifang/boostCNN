69ef545f59e498969b5e95209450f5cb97db8ad6
biyifang <57164923+biyifang@users.noreply.github.com>
2020-05-23 15:39:21 -0500

    Update late_subgrid_boostCNN.py

--- late_subgrid_boostCNN.py
+++ late_subgrid_boostCNN.py
@@ -458,7 +458,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
 
 	#Create module for GBM
-	model_2 = torch.load('SVHN_initial_model_' + args.model_save)
+	#model_2 = torch.load('SVHN_initial_model_' + args.model_save)
+	model_2 = torch.load('initial_model_' + args.model_save)
 	#model_list = [copy.deepcopy(model_2) for _ in range(args.num_boost_iter)]
 	#model_2 = oneCNN()
 	#model_2 = mobilenet_v2()
