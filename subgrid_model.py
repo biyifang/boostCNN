6212cef47967bf5b51a6f8ce0b3d20ed436308bf
biyifang <57164923+biyifang@users.noreply.github.com>
2020-05-23 14:08:19 -0500

    Update subgrid_model.py

--- subgrid_model.py
+++ subgrid_model.py
@@ -554,7 +554,8 @@ class GBM(nn.Module):
         if loss:
             return self.mse(g, w)
         else:
-            return g
+            #return g
+            return nn.functional.softmax(g,-1)
             #return self.weak_learners[iteration](x)
         #data already with correct w/label
     #def line_search(self, f, g, data): plane
