b0c0866056927235ce018709da299992d31647ea
biyifang <57164923+biyifang@users.noreply.github.com>
2020-06-05 13:21:20 -0500

    Update GBM_tree.py

--- GBM_tree.py
+++ GBM_tree.py
@@ -8,14 +8,14 @@ np.random.seed(seed)
 random.seed(seed)
 
 train_embedding = np.load('train_embedding.npy')
-train_label = np.load('train_label.npy') + 1
+train_label = np.load('train_label.npy') 
 val_embedding = np.load('val_embedding.npy')
-val_label = np.load('val_label.npy') + 1
+val_label = np.load('val_label.npy') 
 
 dtrain = xgb.DMatrix(train_embedding, label = train_label)
 dval = xgb.DMatrix(val_embedding, label = val_label)
 
-param = {'max_depth': 3, 'eta': 1, 'objective': 'multi:softprob'}
+param = {'max_depth': 3, 'eta': 1, 'objective': 'multi:softprob', 'num_class': 10}
 
 watchlist = [(dval, 'eval'), (dtrain, 'train')]
 num_round = 10
