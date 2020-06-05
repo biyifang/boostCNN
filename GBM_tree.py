f097fcea6566e2c42b4530ac8668b9be05dbd1c4
biyifang <57164923+biyifang@users.noreply.github.com>
2020-06-05 16:17:08 -0500

    Update GBM_tree.py

--- GBM_tree.py
+++ GBM_tree.py
@@ -15,7 +15,7 @@ val_label = np.load('val_label.npy')
 dtrain = xgb.DMatrix(train_embedding, label = train_label)
 dval = xgb.DMatrix(val_embedding, label = val_label)
 
-param = {'max_depth': 3, 'eta': 0.4, 'objective': 'multi:softprob', 'num_class': 10}
+param = {'max_depth': 3, 'eta': 0.6, 'objective': 'multi:softprob', 'num_class': 10}
 
 watchlist = [(dval, 'eval'), (dtrain, 'train')]
 num_round = 10
