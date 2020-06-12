3c59db0eaf529aa0c5282044f9fafac2073e3319
biyifang <57164923+biyifang@users.noreply.github.com>
2020-06-11 21:24:19 -0500

    Update late_subgrid_boostCNN_resNet18.py

--- late_subgrid_boostCNN_resNet18.py
+++ late_subgrid_boostCNN_resNet18.py
@@ -505,7 +505,7 @@ def main_worker(gpu, ngpus_per_node, args):
 	'''
 
 	model.cpu()
-	output_file = open(args.sample_prob + 'out.txt','w')
+	output_file = open('out.txt'+str(args.sample_prob),'w')
 	
 
 
